f23b10c8f653f7a6fe5657ee1a7d7f503ef88ee8
Governor-LCJ <1781675572@qq.com>
2019-10-28 21:39:03 +0800

    map

---
+++ node/spider-ajax-content/puppeteer.js
@@ -0,0 +1,16 @@
+//无头浏览器
+//软件自动化测试
+const puppeteer = require('puppeteer');
+run();
+async function run() {
+    const browser = await puppeteer.launch({
+        headless:false
+    })
+    const page =await browser.newPage();
+    await page.goto('http://juejin.im/books', {
+        waitUntil:'networkidle0'
+    })
+    await page.screenshot({
+        path:'./book.png'
+    })
+}
