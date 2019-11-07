626f90d5cd6fe77b8566976d68b43486eff997a9
Governor-LCJ <1781675572@qq.com>
2019-11-07 15:58:13 +0800

    drag拖拽

--- node/email-send/index.js
+++ node/email-send/index.js
@@ -1,6 +1,9 @@
 const http =require('http');
 const fs =require('fs');
 const urlModule =require('url');
+const nodemailer = require('nodemailer')
+const code = 23333;
+
 
 // /register 展示 一个 注册html
 //注册成功之后 展示 一个 成功的html
@@ -18,11 +21,32 @@ http.createServer((req,res)=>{
     }else if(urlObj.pathname === '/sendEmail' && method ==='GET'){
         const emailQs =urlObj.query;
         const email =emailQs.split('=')[1];
+        const transport = nodemailer.createTransport({
+            service:'qq',
+            port:465,
+            secureConnection:true,
+            auth:{
+                user:'1781675572@qq.com',
+                pass:'dmsopdvamukfbabf'
+            }
+        })
+        transport.sendMail({
+            to:email,
+            html:`<h2>${code}</h2>`,
+            from:'1781675572@qq.com',
+            subject:'xxx验证'
+        }, (err, info) => {
+            if(err) {
+
+            } else{
+
+            }
+        })
         res.writeHead(200,{
             'Content-Type':'application/json'
         })
         res.end(JSON.stringify({
-            code:200,
+            code:500,
             email
         }))
     }else{
