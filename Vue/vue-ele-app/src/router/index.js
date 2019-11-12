9bdf944aed379f9ab9448ce73758aa1831171ecc
Governor-LCJ <1781675572@qq.com>
2019-11-12 21:02:52 +0800

    node jsonp

--- Vue/vue-ele-app/src/router/index.js
+++ Vue/vue-ele-app/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
 import Goods from '@/components/goods/goods'
+import Ratings from '@/components/ratings/ratings'
+import Seller from '@/components/seller/seller'
 
 Vue.use(Router)
 
@@ -16,6 +18,16 @@ export default new Router({
       path: '/goods',
       name: 'Goods',
       component: Goods
+    },
+    {
+      path: '/ratings',
+      name: 'Ratings',
+      component: Ratings
+    },
+    {
+      path: '/seller',
+      name: 'seller',
+      component: Seller
     }
   ]
 })
