fbd216175e7975809d9654883032e20d1aa34c18
Governor-LCJ <1781675572@qq.com>
2019-11-20 17:08:41 +0800

    vue-notification

---
+++ Vue/vue-notification/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <HelloWorld/> -->
+    <Header />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: red;
+  margin-top: 60px;
+}
+</style>
---
+++ Vue/vue-notification/src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="footer">footer</div>
+</template>
+
+<script>
+import notification from "./notification.js"
+
+export default {
+  mounted() {
+    setTimeout(() => {
+      open({
+        title: "title1",
+        content: "content1"
+      });
+    }, 3000);
+  }
+};
+</script>
+
+<style scoped>
+.footer {
+  position: fixed;
+  bottom: 0;
+  color: green;
+}
+</style>
