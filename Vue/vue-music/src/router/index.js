dcd2feb11919bf8a2869bd21e521b78af108e8cb
Governor-LCJ <1781675572@qq.com>
2019-11-21 14:40:04 +0800

    vue-music

--- Vue/vue-music/src/router/index.js
+++ Vue/vue-music/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Header from '@/components/header'
+import Search from '@/components/pages/search'
 
 Vue.use(Router)
 
@@ -8,8 +8,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Header',
-      component: Header
+      name: 'Search',
+      component: Search
+    },
+    {
+      path: '/search',
+      name: 'Search',
+      component: Search
     }
   ]
 })
